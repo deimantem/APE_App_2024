a2459fb85194ceb047557d02eadcd620f739f60f
deimantem <deimante.mileryte2@gmail.com>
2024-07-07 12:50:26 +0200

    Fix and cleanup tests

--- MLZApp/Maui2024/Core.Tests/MainPageViewModelTests.cs
+++ MLZApp/Maui2024/Core.Tests/MainPageViewModelTests.cs
@@ -1,171 +1,142 @@
 using Core.Services;
 
-namespace Core.Tests
+namespace Core.Tests;
+
+[TestFixture]
+public class MainPageViewModelTests : TestsBase
 {
-    [TestFixture]
-    public class MainPageViewModelTests : TestsBase
+    private static async Task<MainPageViewModel> GetMainPageViewModel(IServiceProvider serviceProvider)
     {
-        [Test]
-        public async Task TestSettingName()
+        var result = serviceProvider.GetRequiredService<MainPageViewModel>();
+        await result.EnsureModelLoaded();
+        return result;
+    }
+
+    private async Task<MainPageViewModel> GetMainPageViewModel()
+    {
+        var serviceProvider = CreateServiceProvider();
+
+        var result = await GetMainPageViewModel(serviceProvider);
+
+        result.Price = 100;
+        result.Matriculation = "M-001";
+        result.Name = "Glider1";
+
+        return result;
+    }
+
+    [Test]
+    public async Task TestSettingName()
+    {
+        var viewModel = await GetMainPageViewModel();
+
+        Assert.That(viewModel.Name, Is.EqualTo("Glider1"));
+
+        viewModel.Name = "Glider2";
+
+        Assert.That(viewModel.Name, Is.EqualTo("Glider2"));
+    }
+
+    [Test]
+    public async Task TestSettingMatriculation()
+    {
+        var viewModel = await GetMainPageViewModel();
+
+        Assert.That(viewModel.Matriculation, Is.EqualTo("M-001"));
+
+        viewModel.Matriculation = "M-002";
+
+        Assert.That(viewModel.Matriculation, Is.EqualTo("M-002"));
+    }
+
+    [Test]
+    public async Task TestIncrementPriceTriggersChange()
+    {
+        var viewModel = await GetMainPageViewModel();
+
+        var notifications = new List<string?>();
+        viewModel.PropertyChanged += (_, args) => notifications.Add(args.PropertyName);
+
+        // Check initial Price value
+        Assert.That(viewModel.Price, Is.EqualTo(100));
+
+        viewModel.IncrementPrice();
+
+        Assert.That(viewModel.Price, Is.EqualTo(101));
+
+        // Ensure PropertyChanged event was raised for Price
+        Assert.That(notifications, Is.EquivalentTo(new[] { "Price" }));
+    }
+
+    protected override IServiceCollection AddServices(IServiceCollection serviceCollection)
+    {
+        return base.AddServices(serviceCollection)
+            .AddSingleton(new LocalStorageSettings { DatabaseFilename = "Maui2024Tests.db3" })
+            .AddSingleton<ILocalStorage<SailplaneModel>, MockLocalStorage<SailplaneModel>>()
+            .AddSingleton<MainPageViewModel>();
+    }
+
+    private class MockLocalStorage<T> : ILocalStorage<T> where T : SailplaneModel, new()
+    {
+        private readonly List<T> _mockData = new()
         {
-            var viewModel = await GetMainPageViewModel();
+            new T { Id = 1, Name = "Glider1", Matriculation = "M-001", Price = 100 },
+            new T { Id = 2, Name = "Glider2", Matriculation = "M-002", Price = 150 },
+            new T { Id = 3, Name = "Glider3", Matriculation = "M-003", Price = 200 }
+        };
 
-            Assert.That(viewModel.Name, Is.EqualTo("Glider1"));
-
-            viewModel.Name = "Glider2";
-
-            Assert.That(viewModel.Name, Is.EqualTo("Glider2"));
+        public Task Initialize()
+        {
+            return Task.CompletedTask;
         }
 
-        [Test]
-        public async Task TestSettingMatriculation()
+        public Task<bool> Save(T item)
         {
-            var viewModel = await GetMainPageViewModel();
-
-            Assert.That(viewModel.Matriculation, Is.EqualTo("M-001"));
-
-            viewModel.Matriculation = "M-002";
-
-            Assert.That(viewModel.Matriculation, Is.EqualTo("M-002"));
-        }
-
-        [Test]
-        public async Task TestIncrementPriceTriggersChange()
-        {
-            var viewModel = await GetMainPageViewModel();
-
-            var notifications = new List<string?>();
-
-            viewModel.PropertyChanged += (_, args) => notifications.Add(args.PropertyName);
-
-            Assert.That(viewModel.Price, Is.EqualTo(100));
-
-            viewModel.IncrementPrice();
-
-            Assert.That(viewModel.Price, Is.EqualTo(101));
-            Assert.That(notifications, Is.EquivalentTo(new[] { "Price" }));
-        }
-
-        // [Test]
-        // public async Task TestSave()
-        // {
-        //     var serviceProvider = CreateServiceProvider();
-        //     var localStorage = serviceProvider.GetRequiredService<ILocalStorage<SailplaneModel>>();
-        //
-        //     await localStorage.DeleteAll();
-        //
-        //     var viewModel = await GetMainPageViewModel(serviceProvider);
-        //
-        //     await viewModel.Save();
-        //
-        //     var settingsModels = await localStorage.LoadAll();
-        //
-        //     Assert.That(settingsModels, Has.Count.EqualTo(1));
-        //     Assert.That(settingsModels[0].Name, Is.EqualTo("Glider1"));
-        //     Assert.That(settingsModels[0].Matriculation, Is.EqualTo("M-001"));
-        //     Assert.That(settingsModels[0].Price, Is.EqualTo(100));
-        // }
-
-        // [Test]
-        // public async Task TestEnsureModelLoaded()
-        // {
-        //     var serviceProvider = CreateServiceProvider();
-        //     var viewModel = serviceProvider.GetRequiredService<MainPageViewModel>();
-        //     var localStorage = serviceProvider.GetRequiredService<ILocalStorage<SailplaneModel>>();
-        //
-        //     await localStorage.DeleteAll();
-        //
-        //     var notifications = new List<string?>();
-        //
-        //     viewModel.PropertyChanged += (_, args) => notifications.Add(args.PropertyName);
-        //
-        //     await viewModel.EnsureModelLoaded();
-        //
-        //     Assert.That(notifications, Is.EquivalentTo(new[] { "SelectedItem", "Name", "FullName", "Matriculation", "FullName", "Price", "IsReady" }));
-        // }
-
-        private async Task<MainPageViewModel> GetMainPageViewModel()
-        {
-            var serviceProvider = CreateServiceProvider();
-
-            return await GetMainPageViewModel(serviceProvider);
-        }
-
-        private static async Task<MainPageViewModel> GetMainPageViewModel(IServiceProvider serviceProvider)
-        {
-            var result = serviceProvider.GetRequiredService<MainPageViewModel>();
-
-            await result.EnsureModelLoaded();
-
-            return result;
-        }
-
-        protected override IServiceCollection AddServices(IServiceCollection serviceCollection)
-        {
-            return base.AddServices(serviceCollection)
-                .AddSingleton(new LocalStorageSettings { DatabaseFilename = "Maui2024Tests.db3" })
-                .AddSingleton<ILocalStorage<SailplaneModel>, MockLocalStorage<SailplaneModel>>() // Use MockLocalStorage for testing
-                .AddSingleton<MainPageViewModel>();
-        }
-
-        private class MockLocalStorage<T> : ILocalStorage<T> where T : SailplaneModel, new()
-        {
-            private readonly List<T> _mockData = new()
+            var existingItem = _mockData.FirstOrDefault(i => i.Id == item.Id);
+            
+            if (existingItem != null)
             {
-                new T { Id = 1, Name = "Glider1", Matriculation = "M-001", Price = 100 },
-                new T { Id = 2, Name = "Glider2", Matriculation = "M-002", Price = 150 },
-                new T { Id = 3, Name = "Glider3", Matriculation = "M-003", Price = 200 }
-            };
-
-            public Task Initialize()
-            {
-                return Task.CompletedTask;
+                existingItem.Name = item.Name;
+                existingItem.Matriculation = item.Matriculation;
+                existingItem.Price = item.Price;
             }
-
-            public Task<bool> Save(T item)
+            else
             {
-                var existingItem = _mockData.FirstOrDefault(i => i.Id == item.Id);
-                if (existingItem != null)
-                {
-                    existingItem.Name = item.Name;
-                    existingItem.Matriculation = item.Matriculation;
-                    existingItem.Price = item.Price;
-                }
-                else
-                {
-                    item.Id = _mockData.Max(i => i.Id ?? 0) + 1;
-                    _mockData.Add(item);
-                }
-                return Task.FromResult(true);
+                item.Id = _mockData.Max(i => i.Id ?? 0) + 1;
+
+                _mockData.Add(item);
             }
+            return Task.FromResult(true);
+        }
 
-            public Task<bool> Delete(T item)
-            {
-                var existingItem = _mockData.FirstOrDefault(i => i.Id == item.Id);
-                if (existingItem != null)
-                {
-                    _mockData.Remove(existingItem);
-                    return Task.FromResult(true);
-                }
-                return Task.FromResult(false);
-            }
+        public Task<bool> Delete(T item)
+        {
+            var existingItem = _mockData.FirstOrDefault(i => i.Id == item.Id);
 
-            public Task<T?> TryLoad(int id)
+            if (existingItem != null)
             {
-                return Task.FromResult(_mockData.FirstOrDefault(i => i.Id == id));
-            }
-
-            public Task<List<T>> LoadAll()
-            {
-                return Task.FromResult(_mockData);
-            }
-
-            public Task<bool> DeleteAll()
-            {
-                _mockData.Clear();
+                _mockData.Remove(existingItem);
 
                 return Task.FromResult(true);
             }
+            return Task.FromResult(false);
+        }
+
+        public Task<T?> TryLoad(int id)
+        {
+            return Task.FromResult(_mockData.FirstOrDefault(i => i.Id == id));
+        }
+
+        public Task<List<T>> LoadAll()
+        {
+            return Task.FromResult(_mockData);
+        }
+
+        public Task<bool> DeleteAll()
+        {
+            _mockData.Clear();
+
+            return Task.FromResult(true);
         }
     }
 }
